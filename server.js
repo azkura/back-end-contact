c91a1e4e6b580527f85e488f4b81eb65ffbd99f8
github_username <azizkoura@hotmail.com>
2019-12-09 15:44:12 +0100

    contact model & route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact API...' }));
 // DEFINE OUR ROUTES
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
