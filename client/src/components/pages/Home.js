53fa27cedba816c9ae86fb5a9db87d86112f64c2
github_username <azizkoura@hotmail.com>
2019-12-10 11:23:05 +0100

    contact item ui

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Contacts } from '../contacts/Contacts';
+import ContactState from '../../context/contact/ContactState';
 
 export const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className='grid-2'>
+      <div>{/* contactform */}</div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
 };
