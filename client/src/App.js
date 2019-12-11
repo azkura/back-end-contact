295059fc6195f9614071013cbda0e53ab6f7e723
github_username <azizkoura@hotmail.com>
2019-12-11 10:36:25 +0100

    alert context

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,35 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import AuthState from './context/auth/AuthState';
-import ContactState from './context/contact/ContactState';
 import { Navbar } from './components/layout/Navbar';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
 
+import AuthState from './context/auth/AuthState';
+import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
+
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/' component={Home} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/' component={Home} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
